49b6ff539e1f5b8dc8b666ca2129f4682820c12a
Oliver Tiebe <oliver.tiebe@gmail.com>
2023-07-11 18:40:50 +0200

    Update

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+$days = 7;
+
 function automaticRedirect(){
 	echo '<meta ';
 	if($_GET['input']){
@@ -48,10 +50,25 @@ function echo_header(){
 	echo '<!DOCTYPE html>';
 	echo '<html lang="de">';
 	echo '<head>';
+	echo "\r\n";
 	
 	automaticRedirect();
+	echo "\r\n";
 	
 	stylesheet();
+	echo "\r\n";
+	
+	echo '<script>
+		function myFunction(category) {
+			var table = document.getElementById(category);
+			if (table.style.display === "none") {
+				table.style.display = "inline";
+			} else {
+				table.style.display = "none";
+			}
+		}
+	</script>';
+	echo "\r\n";
 	
 	echo '</head>';
 	echo '<body>';
@@ -128,7 +145,7 @@ function save_todo(){
 	}
 }
 
-function read_todo(){
+function read_todo($days){
 	$fileName = "notDone.txt";
 		
 	$notDone = '';
@@ -148,48 +165,76 @@ function read_todo(){
 	
 	
 	echo '<form action="index.php?input=modify" method="post">';
-	echo 'Auswahl: ';
+	echo "\r\n";
+	echo '<div id="test" style="display:inline">Auswahl: </div>';
+	echo "\r\n";
 	echo '<button name="type" type="submit" value="delete" >Entfernen</button>';
+	echo "\r\n";
 	echo '<br><br>Verschieben um: ';
+	echo "\r\n";
 	echo '<input name="count" type="text" size="1" autocomplete="off" value="1" >';
+	echo "\r\n";
 	echo '<button name="type" type="submit" value="day" >Tag</button>';
+	echo "\r\n";
 	echo '<button name="type" type="submit" value="week" >Woche</button>';
+	echo "\r\n";
 	echo '<button name="type" type="submit" value="month" >Monat</button>';
+	echo "\r\n";
 	echo '<button name="type" type="submit" value="year" >Jahr</button>';
+	echo "\r\n";
 	echo '<table>';
+	echo "\r\n";
 	for($i=0;$i < count($categories); $i++){
 		$tmp = 0;
 		for($j=0;$j<count($notDone);$j++){
 			if(strcmp($notDone[$j][0],$categories[$i]) == 0){
-			
 			//Zeitraum Filter
 			
-				$days = 7;
-				if(strcmp("HEUTE WICHTIG",$categories[$i]) == 0){
-					$days = 2;
+				$daysTmp = $days;
+				$readMoreCount = 6;
+				
+				if(strcmp("HEUTE WICHTIG",$categories[$i]) == 0/* || strcmp("Urlaub",$categories[$i]) == 0*/){
+					$daysTmp = 2;
+					$readMoreCount = 9999;
 				}
-				if(strcmp("Arzttermine",$categories[$i]) == 0 || strcmp("Termine",$categories[$i]) == 0){
-					$days = 9999;
+				if(strcmp("Arzttermine",$categories[$i]) == 0 || strcmp("Termine",$categories[$i]) == 0 || strcmp("Urlaub",$categories[$i]) == 0){
+					$daysTmp = 9999;
+				}
+				if(strcmp("Anschaffungen",$categories[$i]) == 0 || strcmp("Hochzeit",$categories[$i]) == 0 || strcmp("Urlaub",$categories[$i]) == 0){
+					$readMoreCount = 1;
 				}
 				if($_GET['days']){
-					$days = $_GET['days'];
+					$daysTmp = $_GET['days'];
 				}
 				
 				$today = mktime(0,0,0);
 				
-				if($today + (86400*$days) > $notDone[$j]['1']){
+				if($today + (86400*$daysTmp) > $notDone[$j]['1']){
 					if($tmp == 0){
 						echo '<tr><th colspan="4">'.$categories[$i].'</th></tr>';
-						$tmp++;
+						echo "\r\n";
 					}
-				
+					$tmp++;
+					if($tmp == $readMoreCount){
+						echo '<tr><td colspan="4"><div id="table'.$categories[$i].'" style="display:none"><table>';
+						echo "\r\n";
+					}
 					echo '<tr>';
 						echo '<td width="10"><input type="radio" id="'.$j.'" name="modify" value="'.$j.'"></td>';
 						echo '<td width="10">'.date("d.m.Y",$notDone[$j]['1']).'</td>';
 						echo '<td width="10">&nbsp;</td><td>'.$notDone[$j]['2'].'</td>';
 					echo '</tr>';
+					echo "\r\n";
 				}
 			}
+		}
+		if ($tmp >= $readMoreCount){
+			echo '</table></div></tr></td>';
+			echo "\r\n";
+			echo '<tr><td colspan="4">';
+			echo '<a id="myLink" href="#" onclick="'."myFunction('table".$categories[$i]."')".';return false;">Mehr Anzeigen</a>';
+			echo '</td></tr>';
+			echo "\r\n";
 		}
 	}
 	echo '</table>';
@@ -197,7 +242,8 @@ function read_todo(){
 }
 
 function done_todo(){
-	if ($_GET['input'] == "modify" && count($_POST) > 0){
+	if ($_GET['input'] == "modify" && count($_POST) > 0 && $_POST != array('count'=>1)){
+		print_r($_POST);
 		$fileName = "notDone.txt";
 		
 		$notDone = '';
@@ -255,10 +301,10 @@ function done_todo(){
 }
 
 
-function setFilter(){
+function setFilter($days){
 	echo '<form action="index.php" method="get">';
 	echo '<label for="Zeitfilter">Zeitfilter:</label><br>';
-	echo '<input type="number" id="days" name="days" autocomplete="off" min="1" value="7">';
+	echo '<input type="number" id="days" name="days" autocomplete="off" min="1" value="'.$days.'">';
 	echo '<button type="submit">Eingaben absenden</button>';
 	echo '</form>';
 }
@@ -271,9 +317,9 @@ done_todo();
 
 enter_todo();
 
-read_todo();
+read_todo($days);
 
-setFilter();
+setFilter($days);
 
 footer();
 
